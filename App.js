ea4dc169ebf081d9f1dcbcec985b2f1bcde56caf
Dai <dainpse150682@fpt.edu.vn>
2024-01-25 10:36:52 +0700

    up code

--- App.js
+++ App.js
@@ -1,40 +1,64 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Ionicons } from '@expo/vector-icons';
-import HomeScreen from './src/page/Home';
-import LoginScreen from './src/page/Login';
-import SignUpScreen from './src/page/SignUp';
-import { IntroSlider } from './src/page/Intro';
-import { Survey } from './src/page/Survey';
-import { Notification } from './src/page/notification/Notification';
-import UpImg from './src/Components/UpImg';
-import { View } from 'native-base';
-import { StyleSheet } from 'react-native';
-import { NativeBaseConfigProvider } from 'native-base';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
+import HomeScreen from "./src/page/Home";
+import LoginScreen from "./src/page/Login";
+import SignUpScreen from "./src/page/SignUp";
+import { IntroSlider } from "./src/page/Intro";
+import { Survey } from "./src/page/Survey";
+import { Notification } from "./src/page/notification/Notification";
+import UpImg from "./src/Components/UpImg";
+import { View } from "native-base";
+import { StyleSheet } from "react-native";
+import { NativeBaseConfigProvider } from "native-base";
+import ProfileSettingScreen from "./src/page/Profile";
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
 const App = () => {
   return (
-  
-        <NavigationContainer>
-          <Stack.Navigator initialRouteName="Login">
-            <Stack.Screen name="Login" options={{ headerLeft: null, headerShown: false }} component={LoginScreen} />
-            <Stack.Screen name="SignUp" options={{ headerLeft: null, headerShown: false }} component={SignUpScreen} />
-            <Stack.Screen name="Homes" options={{ headerLeft: null, headerShown: false }} component={MyBottomNavigationBar} />
-            <Stack.Screen name="IntroSlider" options={{ headerLeft: null, headerShown: false }} component={IntroSlider} />
-            <Stack.Screen name="Survey" options={{ headerLeft: null, headerShown: false }} component={Survey} />
-          </Stack.Navigator>
-        </NavigationContainer>
-
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen
+          name="Login"
+          options={{ headerLeft: null, headerShown: false }}
+          component={LoginScreen}
+        />
+        <Stack.Screen
+          name="SignUp"
+          options={{ headerLeft: null, headerShown: false }}
+          component={SignUpScreen}
+        />
+        <Stack.Screen
+          name="Homes"
+          options={{ headerLeft: null, headerShown: false }}
+          component={MyBottomNavigationBar}
+        />
+        <Stack.Screen
+          name="IntroSlider"
+          options={{ headerLeft: null, headerShown: false }}
+          component={IntroSlider}
+        />
+        <Stack.Screen
+          name="Survey"
+          options={{ headerLeft: null, headerShown: false }}
+          component={Survey}
+        />
+        <Stack.Screen
+          name="Profile"
+          options={{ headerLeft: null, headerShown: false }}
+          component={ProfileSettingScreen}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#FFF',  // Đặt màu nền trắng cho toàn bộ ứng dụng
+    backgroundColor: "#FFF", // Đặt màu nền trắng cho toàn bộ ứng dụng
   },
 });
 function MyBottomNavigationBar() {
@@ -43,26 +67,42 @@ function MyBottomNavigationBar() {
       screenOptions={({ route }) => ({
         tabBarIcon: ({ focused, color, size }) => {
           let iconName;
-          if (route.name === 'Home') {
-            iconName = focused ? 'ios-home' : 'ios-home-outline';
-          } else if (route.name === 'Search') {
-            iconName = focused ? 'ios-search' : 'ios-search-outline';
-          } else if (route.name === 'Profile') {
-            iconName = focused ? 'ios-person' : 'ios-person-outline';
-          } else if (route.name === 'Notification') {
-            iconName = focused ? 'notifications' : 'notifications-outline';
-          } else if (route.name === 'Clinics') {
-            iconName = focused ? 'clipboard' : 'clipboard-outline';
+          if (route.name === "Home") {
+            iconName = focused ? "ios-home" : "ios-home-outline";
+          } else if (route.name === "Search") {
+            iconName = focused ? "ios-search" : "ios-search-outline";
+          } else if (route.name === "Profile") {
+            iconName = focused ? "ios-person" : "ios-person-outline";
+          } else if (route.name === "Notification") {
+            iconName = focused ? "notifications" : "notifications-outline";
+          } else if (route.name === "Clinics") {
+            iconName = focused ? "clipboard" : "clipboard-outline";
           }
           return <Ionicons name={iconName} size={size} color={color} />;
         },
       })}
       keyboardShouldPersistTaps="handled"
     >
-      <Tab.Screen name="Home" options={{ headerShown: false }} component={HomeScreen} />
-      <Tab.Screen name="Clinics" options={{ headerShown: false }} component={Survey} />
-      <Tab.Screen name="Notification" options={{ headerShown: false }} component={Notification} />
-      <Tab.Screen name="Profile" options={{ headerShown: false }} component={UpImg} />
+      <Tab.Screen
+        name="Home"
+        options={{ headerShown: false }}
+        component={HomeScreen}
+      />
+      <Tab.Screen
+        name="Clinics"
+        options={{ headerShown: false }}
+        component={Survey}
+      />
+      <Tab.Screen
+        name="Notification"
+        options={{ headerShown: false }}
+        component={Notification}
+      />
+      <Tab.Screen
+        name="Profile"
+        options={{ headerShown: false }}
+        component={UpImg}
+      />
     </Tab.Navigator>
   );
 }
---
+++ src/page/Profile.js
@@ -0,0 +1,175 @@
+import React, { useState } from "react";
+import { StyleSheet, View, ScrollView } from "react-native";
+import { Avatar, Text, List, Icon, IconButton } from "react-native-paper";
+export default function ProfileSettingScreen({ navigation }) {
+  const [expanded, setExpanded] = useState({});
+
+  const list2 = [
+    {
+      name: "Show Collection",
+
+      icon: "dog",
+    },
+    {
+      name: "Change Password",
+
+      icon: "fingerprint",
+    },
+    {
+      name: "New User",
+      avatar_url: "https://example.com/avatar3.jpg",
+      subtitle: "New Role",
+      icon: "credit-card",
+    },
+    {
+      name: "New User",
+      avatar_url: "https://example.com/avatar3.jpg",
+      subtitle: "New Role",
+      icon: "lifebuoy",
+    },
+  ];
+  const log = (item) => {
+    // Only navigate when "Change Password" is pressed
+    if (item.name === "Change Password") {
+      navigation.navigate("Password", { item });
+    }
+  };
+
+  const toggleAccordion = (index) => {
+    setExpanded((prevExpanded) => {
+      return { ...prevExpanded, [index]: !prevExpanded[index] };
+    });
+  };
+
+  return (
+    <ScrollView
+      style={{ backgroundColor: "#F6F6F6" }}
+      contentContainerStyle={styles.container}
+    >
+      <Text
+        style={{
+          fontSize: 26,
+          textAlign: "center",
+          margin: 50,
+          fontWeight: "bold",
+        }}
+      >
+        Profile
+      </Text>
+      <View style={{ margin: 0 }} />
+      <View style={{ flexDirection: "row", alignItems: "center" }}>
+        <Avatar.Image
+          size={100}
+          source={{ uri: "https://example.com/avatar.jpg" }}
+          style={{ marginLeft: 25 }}
+        />
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+            width: "100%",
+            paddingRight: 150,
+          }}
+        >
+          <View>
+            <Text
+              style={{
+                marginBottom: 30,
+                fontSize: 28,
+                fontWeight: "bold",
+                marginLeft: 7,
+              }}
+            >
+              Your Name
+            </Text>
+            <Text style={{ marginTop: 10, fontSize: 16, marginLeft: 7 }}>
+              {" "}
+              Date Year
+            </Text>
+          </View>
+          <IconButton
+            icon="pen"
+            size={35}
+            style={{
+              marginBottom: 60,
+            }}
+            onPress={() => navigation.navigate("ChangeProfile")}
+          />
+        </View>
+      </View>
+      <View style={{ height: 35 }} />
+
+      {list2.map((l, i) => (
+        <List.Accordion
+          key={i}
+          title={
+            i === 0
+              ? "My Pet"
+              : i === 1
+              ? "Password"
+              : i === 2
+              ? "Payment Method"
+              : "Support"
+          } // Đổi tiêu đề ở đây
+          expanded={expanded[i]}
+          onPress={() => toggleAccordion(i)}
+          style={{
+            backgroundColor: "#F6F6F6",
+            fontWeight: "bold",
+          }}
+          titleStyle={{ fontWeight: "bold", fontSize: 19 }}
+        >
+          <List.Item
+            title={l.name}
+            description={l.subtitle}
+            left={(props) => <List.Icon {...props} icon={l.icon} />}
+            onPress={() => log(l)}
+            titleStyle={styles.titleStyle}
+          />
+        </List.Accordion>
+      ))}
+      <List.Item
+        title="Logout"
+        right={(props) => <List.Icon {...props} icon="logout" />}
+        onPress={log}
+        titleStyle={{ fontSize: 19, fontWeight: "bold" }}
+      />
+      <View style={styles.updateAccountContainer}>
+        <List.Item
+          title={
+            <Text style={styles.updateAccountText}>Update Your Account</Text>
+          }
+          onPress={log}
+          style={styles.updateAccount}
+        />
+      </View>
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: "center",
+    padding: 16,
+  },
+  updateAccountContainer: {
+    height: 200,
+    justifyContent: "center",
+    width: 220,
+    height: 140,
+    marginLeft: 75,
+    marginVertical: 10,
+  },
+  updateAccount: {
+    backgroundColor: "#C660F6",
+    alignItems: "center",
+    borderRadius: 10,
+  },
+  updateAccountText: {
+    textAlign: "center",
+    fontSize: 18,
+    color: "white",
+    fontWeight: 700,
+  },
+});

88b2436932c3495e46a05ebf99f8681730b6fdc0
MinhTri <hminhtri03@gmail.com>
2024-01-31 23:32:33 +0700

    add-API

--- src/page/Survey.js
+++ src/page/Survey.js
@@ -4,19 +4,19 @@ import { Button, TextInput } from 'react-native-paper';
 import { DatePickerInput } from 'react-native-paper-dates';
 import Swiper from 'react-native-swiper';
 import { SelectList } from 'react-native-dropdown-select-list'
-import { Slider, Icon } from '@rneui/themed';
+import DateTimePicker from "@react-native-community/datetimepicker";
+import * as ImagePicker from "expo-image-picker";
+import { firebaseImg } from '../api/firebaseImg';
 export const Survey = ({ navigation }) => {
   const swiperRef = useRef(null);
-  const [option, setOption] = useState('');
-  const [inputDate, setInputDate] = React.useState(undefined)
   const handleDone = () => {
     navigation.navigate('Homes');
     // Handle done logic
   };
 
   const SliderComponent = () => {
+    const [hasExperience, setHasExperience] = useState(null);
     const [selected, setSelected] = React.useState("");
-    const [value, setValue] = useState(0);
     const selectData = [
       { key: '1', value: 'Mobiles' },
       { key: '2', value: 'Appliances' },
@@ -26,8 +26,35 @@ export const Survey = ({ navigation }) => {
       { key: '6', value: 'Diary Products' },
       { key: '7', value: 'Drinks' },
     ]
+    const [dob, setDob] = useState("2000-2-1T20:35:52.184Z");
+    console.log(dob)
+    const [showDatePicker, setShowDatePicker] = useState(false);
+    const displayDatePicker = () => {
+      setShowDatePicker(true);
+    };
+
+    const handleDateChange = (event, selectedDate) => {
+      const currentDate = selectedDate || dob;
+      setShowDatePicker(false);
+      setDob(currentDate);
+    };
+    const [petAvatar,setPetAvatar] = useState(null);
+    console.log(dob);
+    const handleChoosePhoto = async () => {
+      let result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.All,
+        allowsEditing: true,
+        aspect: [4, 3],
+        quality: 1,
+      });
 
 
+      if (!result.canceled) {
+        const response = await firebaseImg(result)
+        setPetAvatar(response);
+      }
+
+    };
     return (
       <View style={styles.wrapper}>
         <Swiper
@@ -53,28 +80,35 @@ export const Survey = ({ navigation }) => {
               </View>
               <View>
                 <Text style={styles.quizText}>Do you have and experience with pet ?</Text>
-                <View style={{ flexDirection: 'row', gap: 50, alignItems: "center", marginLeft: 50 }}>
-                  <Button
-                    style={option === 'Yes' ? styles.optionhightlight : styles.option}
-                    onPress={() => setOption('Yes')}
-                  >
-                    <Text style={{ color: option === 'Yes' ? '#F6F6F6' : 'black' }}>Yes</Text>
-                  </Button>
-                  <Button
-                    style={option === 'No' ? styles.optionhightlight : styles.option}
-                    onPress={() => setOption('No')}
-                  >
-                    <Text style={{ color: option === 'No' ? '#F6F6F6' : 'black' }}>No</Text>
-                  </Button>
-                </View>
+                <View style={{ flexDirection: 'row', gap: 50, alignItems: 'center', marginLeft: 50 }}>
+              <Button
+                style={hasExperience === true ? styles.optionhightlight : styles.option}
+                onPress={() => setHasExperience(true)}
+              >
+                <Text style={{ color: hasExperience === true ? '#F6F6F6' : 'black' }}>Yes</Text>
+              </Button>
+              <Button
+                style={hasExperience === false ? styles.optionhightlight : styles.option}
+                onPress={() => setHasExperience(false)}
+              >
+                <Text style={{ color: hasExperience === false ? '#F6F6F6' : 'black' }}>No</Text>
+              </Button>
+            </View>
               </View>
               <View>
                 <Text style={styles.quizText}>Give some photo of your pet</Text>
-                <TouchableOpacity>
+                <TouchableOpacity
+                  onPress={handleChoosePhoto}>
+                  {petAvatar ? (
+                    <Image 
+                    // source={{ uri: petAvatar?.assets ? petAvatar.assets[0].uri: 'https://firebasestorage.googleapis.com/v0/b/swd-longchim.appspot.com/o/376577375_998270051209102_4679797004619533760_n.jpg?alt=media&token=90d94961-bc1b-46e4-b60a-ad731606b13b' }}
+                    style = {styles.image}/>
+                  ):(
                   <Image
                     source={require('../../assets/ImgInput.jpg')}
                     style={styles.image}
                   />
+                  )}
                 </TouchableOpacity>
               </View>
               <View>
@@ -87,10 +121,36 @@ export const Survey = ({ navigation }) => {
               </View>
               <View>
                 <Text style={styles.quizText}>What your pet's date of birth</Text>
-                <DatePickerInput
-                  onChange={(d) => setInputDate(d)}
-                  value={inputDate}>
-                </DatePickerInput>
+                <TouchableOpacity onPress={displayDatePicker}>
+                  <TextInput
+                    mode="outlined"
+                    style={{
+                      ...styles.input,
+                      backgroundColor: "#E9E7E7",
+                    }}
+                    onFocus={displayDatePicker}
+                    value={dob ? new Date(new Date(dob).getTime() - 7 * 60 * 60 * 1000).toLocaleDateString() : ""}
+                    placeholder="Select date..."
+                    placeholderTextColor="#24252B"
+                    editable={false}
+                    left={
+                      <TextInput.Icon
+                        icon="calendar"
+                        size={35}
+                        style={{ marginTop: 5 }}
+                      />
+                    }
+                  />
+                </TouchableOpacity>
+                {showDatePicker && (
+                  <DateTimePicker
+                    testID="dateTimePicker"
+                    value={dob ? new Date(new Date(dob).getTime() - 7 * 60 * 60 * 1000) : new Date()}
+                    mode="date"
+                    display="default"
+                    onChange={handleDateChange}
+                  />
+                )}
               </View>
 
               <View style={{ height: 100 }}></View>
@@ -114,38 +174,6 @@ export const Survey = ({ navigation }) => {
               </View>
               <View>
                 <Text style={styles.quizText}>Choose gender of your pet</Text>
-                <View style={{ flexDirection: 'row', gap: 50, alignItems: "center" }}>
-                  <TouchableOpacity style={styles.customButton}>
-                    <Text style={styles.buttonText}>Male</Text>
-                  </TouchableOpacity>
-                  <TouchableOpacity style={styles.customButton}>
-                    <Text style={styles.buttonText}>Female</Text>
-                  </TouchableOpacity>
-                </View>
-              </View>
-              <View style={{ padding: 20 }}>
-                <Slider
-                  value={value}
-                  onValueChange={setValue}
-                  maximumValue={2}
-                  minimumValue={0}
-                  step={1}
-                  allowTouchTrack
-                  trackStyle={{ height: 5, backgroundColor: 'transparent' }}
-                  thumbStyle={{ height: 20, width: 20, backgroundColor: 'transparent' }}
-                  thumbProps={{
-                    children: (
-                      <Icon
-                        name="paw"
-                        type="font-awesome"
-                        size={15}
-                        reverse
-                        containerStyle={{ bottom: 20, right: 20 }}
-                        color="gray"
-                      />
-                    ),
-                  }}
-                />
               </View>
               <View style={{ flexDirection: 'row', gap: 40, justifyContent: 'center', padding: 20 }}>
                 <Text style={styles.buttonText}>Up to 4 moth</Text>
@@ -165,57 +193,57 @@ export const Survey = ({ navigation }) => {
           </View>
           {/* .............................................................................................................. */}
           <View style={styles.slide}>
-            <ScrollView contentContainerStyle={{ marginTop: 50 }}>
-              <View>
-                <Text style={{ fontSize: 32 }}>Help us understand you by answering few question</Text>
+
+            <View>
+              <Text style={{ fontSize: 32 }}>Help us understand you by answering few question</Text>
+            </View>
+            <View>
+              <Text style={styles.quizText}>What kind of your pet chracter ?</Text>
+              <View style={{ flexDirection: 'row', gap: 10, justifyContent: 'center' }}>
+                <TouchableOpacity style={styles.customButton}>
+                  <Text style={styles.buttonText}>Active</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.customButton}>
+                  <Text style={styles.buttonText}>Friendly</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.customButton}>
+                  <Text style={styles.buttonText}>Playful</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.customButton}>
+                  <Text style={styles.buttonText}>Calm</Text>
+                </TouchableOpacity>
               </View>
-              <View>
-                <Text style={styles.quizText}>What kind of your pet chracter ?</Text>
-                <View style={{ flexDirection: 'row', gap: 10, justifyContent: 'center' }}>
-                  <TouchableOpacity style={styles.customButton}>
-                    <Text style={styles.buttonText}>Active</Text>
-                  </TouchableOpacity>
-                  <TouchableOpacity style={styles.customButton}>
-                    <Text style={styles.buttonText}>Friendly</Text>
-                  </TouchableOpacity>
-                  <TouchableOpacity style={styles.customButton}>
-                    <Text style={styles.buttonText}>Playful</Text>
-                  </TouchableOpacity>
-                  <TouchableOpacity style={styles.customButton}>
-                    <Text style={styles.buttonText}>Calm</Text>
-                  </TouchableOpacity>
-                </View>
+            </View>
+            <View>
+              <Text style={styles.quizText}>Vaccinated your pet </Text>
+              <View style={{ flexDirection: 'row', gap: 10 }}>
+                <TouchableOpacity style={styles.customButton}>
+                  <Text style={styles.buttonText}>Yes</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.customButton}>
+                  <Text style={styles.buttonText}>No</Text>
+                </TouchableOpacity>
               </View>
-              <View>
-                <Text style={styles.quizText}>Vaccinated your pet </Text>
-                <View style={{ flexDirection: 'row', gap: 10 }}>
-                  <TouchableOpacity style={styles.customButton}>
-                    <Text style={styles.buttonText}>Yes</Text>
-                  </TouchableOpacity>
-                  <TouchableOpacity style={styles.customButton}>
-                    <Text style={styles.buttonText}>No</Text>
-                  </TouchableOpacity>
-                </View>
+            </View>
+            <View>
+              <Text style={styles.quizText}>Pedigree of your pet</Text>
+              <View style={{ flexDirection: 'row', gap: 10 }}>
+                <TouchableOpacity style={styles.customButton}>
+                  <Text style={styles.buttonText}>Yes</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.customButton}>
+                  <Text style={styles.buttonText}>No</Text>
+                </TouchableOpacity>
               </View>
-              <View>
-                <Text style={styles.quizText}>Pedigree of your pet</Text>
-                <View style={{ flexDirection: 'row', gap: 10 }}>
-                  <TouchableOpacity style={styles.customButton}>
-                    <Text style={styles.buttonText}>Yes</Text>
-                  </TouchableOpacity>
-                  <TouchableOpacity style={styles.customButton}>
-                    <Text style={styles.buttonText}>No</Text>
-                  </TouchableOpacity>
-                </View>
-              </View>
-              <TouchableOpacity onPress={handleDone} style={styles.doneButton}>
-                <Text style={{
-                  color: 'black',
-                  fontSize: 20,
-                  fontWeight: 'bold',
-                }}>Done</Text>
-              </TouchableOpacity>
-            </ScrollView>
+            </View>
+            <TouchableOpacity onPress={handleDone} style={styles.doneButton}>
+              <Text style={{
+                color: 'black',
+                fontSize: 20,
+                fontWeight: 'bold',
+              }}>Done</Text>
+            </TouchableOpacity>
+
           </View>
 
 

eead3627a8f5b4901b0e7fc36b1e71b66591689c
MinhTri <hminhtri03@gmail.com>
2024-02-29 18:36:34 +0700

    mergeMaintoService

--- src/page/Home.js
+++ src/page/Home.js
@@ -899,10 +899,12 @@ const styles = StyleSheet.create({
     fontSize: "10px",
     paddingTop: 28,
     paddingHorizontal: 10,
+    
   },
   container: {
     flex: 1,
     backgroundColor: "#FFFFFF",
+    
   },
   fonts: {
     marginBottom: 8,
